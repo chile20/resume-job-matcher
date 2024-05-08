3e1325e98aa3e33446c61032dcd5d9a14a3c5719
Chi Le <cdle@cpp.edu>
2024-05-08 16:52:17 -0700

    Handle error with missing CSS stylesheets

--- run.py
+++ run.py
@@ -2,5 +2,11 @@ from app import create_app
 
 app = create_app()
 
+# Handling missing CSS
+@app.route('/styles.css')
+@app.route('/styles.css.map')
+def handle_missing_css():
+    return "This resource is not used.", 200
+
 if __name__ == '__main__':
     app.run(debug=True)
